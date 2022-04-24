114a5a36725cfacee0e54b7729c5b4969df4fe05
Dwityantherrys Bekti Utomo <1941720033@student.polinema.ac.id>
2022-04-25 04:20:07 +0700

    rubah versi react

---
+++ Week9/src/services/get.js
@@ -0,0 +1,19 @@
+import { domainPath } from './config';
+
+const GetApi = (path) => {
+  const promise = new Promise((resolve, reject) => {
+    fetch(`${domainPath}/${path}`)
+      .then((response) => response.json())
+      .then(
+        (result) => {
+          resolve(result);
+        },
+        (err) => {
+          reject(err);
+        }
+      );
+  });
+  return promise;
+};
+
+export default GetApi;
